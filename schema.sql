f816e92da366fa2890be19e4229d4a3ae848a1dd
Sandhya Mahendra Mourya <sandhyamaurya123456789@gmail.com>
2026-05-04 17:31:17 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,18 @@
+--Creating Database
+CREATE DATABASE Online_Bookstore;
+
+--Switch to Database
+\c Online_Bookstore;
+
+--Creating Books table 
+DROP TABLE IF EXISTS Books;
+CREATE TABLE Books(
+    Book_ID INT PRIMARY KEY,
+    Title VARCHAR(100),
+    Author VARCHAR(100),
+    Genre VARCHAR(100),
+    Published_Year INT,
+    Price NUMERIC(10,2),
+    Stock INT
+);
 
