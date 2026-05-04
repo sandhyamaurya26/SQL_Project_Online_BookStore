a1f3c7abc84cca524d64ab8015f5adde1a4d76f0
Sandhya Mahendra Mourya <sandhyamaurya123456789@gmail.com>
2026-05-04 17:38:14 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -16,3 +16,49 @@ CREATE TABLE Books(
     Stock INT
 );
 
+--Creating Customers table
+DROP TABLE IF EXISTS Customers;
+CREATE TABLE Customers (
+    Customer_ID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Email VARCHAR(50),        
+    Phone VARCHAR(20),
+    City VARCHAR(50),
+    Country VARCHAR(100)
+);
+
+--Creating table orders
+DROP TABLE IF EXISTS orders;
+CREATE TABLE Orders(
+    Order_ID INT,	
+    Customer_ID	INT REFERENCES CUSTOMERS(Customer_ID),
+    Book_ID	INT REFERENCES BOOKS(Book_ID),
+    Order_Date DATE DEFAULT CURRENT_DATE,
+    Quantity INT,
+    Total_Amount NUMERIC(10,2)
+);
+
+--copying data from csv file 
+COPY BOOKS(Book_ID,Title,Author,Genre,Published_Year,Price,Stock)
+FROM 'C:\SANDHYA\Data Analysis\SQL PROJECT\BOOKS.CSV'
+CSV HEADER;
+
+
+COPY CUSTOMERS(Customer_ID,Name,Email,Phone,City,Country)
+FROM 'C:\SANDHYA\Data Analysis\SQL PROJECT\CUSTOMERS.CSV'
+CSV HEADER;
+
+
+COPY CUSTOMERS(Order_ID,Customer_ID,Book_ID,Order_Date,Quantity,Total_Amount)
+FROM 'C:\SANDHYA\Data Analysis\SQL PROJECT\ORDERS.CSV'
+CSV HEADER;
+
+--Displaying Records
+SELECT * FROM Books LIMIT 10;
+
+SELECT * FROM CUSTOMERS LIMIT 10;
+
+SELECT * FROM orders LIMIT 10;
+
+
+
